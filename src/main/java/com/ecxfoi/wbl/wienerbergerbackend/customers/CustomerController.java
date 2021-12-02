3daa06261656bb22907e6c733730137bda009c62
Mario Nižić <mnizic@foi.hr>
2021-12-02 22:03:41 +0100

    WBL-12 Customer Controller Added a CustomerController that handles requests incoming on route "/api/companies".

---
+++ src/main/java/com/ecxfoi/wbl/wienerbergerbackend/customers/CustomerController.java
@@ -0,0 +1,45 @@
+package com.ecxfoi.wbl.wienerbergerbackend.customers;
+
+import com.ecxfoi.wbl.wienerbergerbackend.models.WienerbergerResponse;
+import com.ecxfoi.wbl.wienerbergerbackend.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerController
+{
+    CustomerService customerService;
+
+    public CustomerController(@Autowired CustomerService customerService)
+    {
+        this.customerService = customerService;
+    }
+
+    @RequestMapping(value = "/api/companies", method = RequestMethod.GET)
+    public ResponseEntity<?> getCompanies()
+    {
+        var context = SecurityContextHolder.getContext().getAuthentication();
+        Long id;
+
+        try
+        {
+            id = (Long)context.getPrincipal();
+            if(id == null){
+                throw new Exception("Invalid ID.");
+            }
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new WienerbergerResponse<>(false, "Invalid credentials!", null));
+        }
+
+        var userCompanies = customerService.getUserCompanies(id);
+        return ResponseEntity.ok(new WienerbergerResponse<>(true, "Success!", userCompanies));
+    }
+}
