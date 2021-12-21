ad1e8f28321a5e89de29416bd8f21dd1d2c96f57
mmustac332 <mmustac@foi.hr>
2021-12-21 03:21:04 +0100

    WBL-40 Ticket Controller Created Ticket Controller which handles GET request incoming on route "/api/tickets/{id}" and GET and POST request incoming on "/api/tickets".

---
+++ src/main/java/com/ecxfoi/wbl/wienerbergerbackend/controllers/TicketController.java
@@ -0,0 +1,96 @@
+package com.ecxfoi.wbl.wienerbergerbackend.controllers;
+
+import com.ecxfoi.wbl.wienerbergerbackend.dto.TicketDto;
+import com.ecxfoi.wbl.wienerbergerbackend.repository.UserRepository;
+import com.ecxfoi.wbl.wienerbergerbackend.response.WienerbergerResponse;
+import com.ecxfoi.wbl.wienerbergerbackend.service.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class TicketController
+{
+    private final TicketService ticketService;
+    private final UserRepository userRepository;
+
+    public TicketController(@Autowired TicketService ticketService, @Autowired UserRepository userRepository)
+    {
+        this.ticketService = ticketService;
+        this.userRepository = userRepository;
+    }
+
+    @RequestMapping(value = "/api/tickets", method = RequestMethod.GET)
+    public  ResponseEntity<?> getTickets()
+    {
+        Authentication context = SecurityContextHolder.getContext().getAuthentication();
+        Long idJWT = context != null ? (Long) context.getPrincipal() : null;
+
+        if(idJWT == null)
+        {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new WienerbergerResponse<>(false, "Invalid credentials!", null));
+        }
+
+        try
+        {
+            List<TicketDto> userTickets = ticketService.getTicketsForUser(idJWT);
+            return ResponseEntity.ok(new WienerbergerResponse<>(true, "Success!", userTickets));
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new WienerbergerResponse<>(false, "Tickets not found!", null));
+        }
+    }
+
+    @RequestMapping(value = "/api/tickets/{id}", method = RequestMethod.GET)
+    public  ResponseEntity<?> getDetailsForTicket(@PathVariable final Long id)
+    {
+        Authentication context = SecurityContextHolder.getContext().getAuthentication();
+        Long idJWT = context != null ? (Long) context.getPrincipal() : null;
+
+        if(idJWT == null)
+        {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new WienerbergerResponse<>(false, "Invalid credentials!", null));
+        }
+
+        try
+        {
+            TicketDto userTicket = ticketService.getTicketForUser(id);
+            return ResponseEntity.ok(new WienerbergerResponse<>(true, "Success!", userTicket));
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new WienerbergerResponse<>(false, "Ticket not found!", null));
+        }
+    }
+
+    @RequestMapping(value = "api/tickets", method = RequestMethod.POST)
+    public ResponseEntity<?> createNewTicket(@RequestBody TicketDto ticketDto)
+    {
+        Authentication context = SecurityContextHolder.getContext().getAuthentication();
+        Long idJWT = context != null ? (Long) context.getPrincipal() : null;
+
+        if(idJWT == null)
+        {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new WienerbergerResponse<>(false, "Invalid credentials!", null));
+        }
+
+        try
+        {
+            ticketService.saveTickedDto(ticketDto);
+            return ResponseEntity.ok(new WienerbergerResponse<>(true, "Success!", ticketDto));
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new WienerbergerResponse<>(false, "Ticket couldn't be added!", null));
+        }
+    }
+}
