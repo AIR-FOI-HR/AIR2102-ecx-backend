b197af01a7584526380695fdbbfb971381c21153
mmustac332 <mmustac@foi.hr>
2021-12-21 03:05:28 +0100

    WBL-40 Ticket Mapper Created mapper for tickets. Mapper is accesed with a Dto and is a better way to convert h2 data to spring boot and vice versa.

---
+++ src/main/java/com/ecxfoi/wbl/wienerbergerbackend/mapper/TicketMapper.java
@@ -0,0 +1,45 @@
+package com.ecxfoi.wbl.wienerbergerbackend.mapper;
+
+import com.ecxfoi.wbl.wienerbergerbackend.dto.TicketDto;
+import com.ecxfoi.wbl.wienerbergerbackend.model.Ticket;
+
+public class TicketMapper implements GenericMapper<TicketDto, Ticket>
+{
+    @Override
+    public TicketDto mapDto(final Ticket entity)
+    {
+        if(entity == null)
+        {
+            return null;
+        }
+        TicketDto ticketDto = new TicketDto();
+
+        ticketDto.setId(entity.getIdTicket());
+        ticketDto.setTicketUser(entity.getTicketUser());
+        ticketDto.setSubject(entity.getSubject());
+        ticketDto.setStatus(entity.getStatus());
+        ticketDto.setMessage(entity.getMessage());
+        ticketDto.setResolve_message(entity.getResolveMessage());
+
+        return ticketDto;
+    }
+
+    @Override
+    public Ticket map(final TicketDto dto)
+    {
+        if(dto == null)
+        {
+            return null;
+        }
+        Ticket ticket = new Ticket();
+
+        ticket.setIdTicket(dto.getId());
+        ticket.setTicketUser(dto.getTicketUser());
+        ticket.setSubject(dto.getSubject());
+        ticket.setStatus(dto.getStatus());
+        ticket.setMessage(dto.getMessage());
+        ticket.setResolveMessage(dto.getResolve_message());
+
+        return ticket;
+    }
+}
