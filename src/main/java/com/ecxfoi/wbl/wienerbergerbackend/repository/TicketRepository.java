54535e89b806315b8a09163e7e6e37a48ed8154e
mmustac332 <mmustac@foi.hr>
2021-12-23 00:17:07 +0100

    WBL-40 Fix Ticket Repository Method findAllByTicketUser returns list of tickets.

--- src/main/java/com/ecxfoi/wbl/wienerbergerbackend/repository/TicketRepository.java
+++ src/main/java/com/ecxfoi/wbl/wienerbergerbackend/repository/TicketRepository.java
@@ -5,10 +5,12 @@ import com.ecxfoi.wbl.wienerbergerbackend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long>
 {
     Ticket findTicketByIdTicket(Long id);
 
-    Ticket findAllByTicketUser(User user);
+    List<Ticket> findAllByTicketUser(User user);
 }
