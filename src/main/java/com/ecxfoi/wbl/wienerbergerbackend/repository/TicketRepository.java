942dc576f2793a8697e37777b84cc9e4b3bcf4e0
mmustac332 <mmustac@foi.hr>
2021-12-22 11:48:45 +0100

    WBL-40 Updating Ticket Repository I have added another method findAllByTicketUser which should return all tickets for speicific user.

--- src/main/java/com/ecxfoi/wbl/wienerbergerbackend/repository/TicketRepository.java
+++ src/main/java/com/ecxfoi/wbl/wienerbergerbackend/repository/TicketRepository.java
@@ -1,6 +1,7 @@
 package com.ecxfoi.wbl.wienerbergerbackend.repository;
 
 import com.ecxfoi.wbl.wienerbergerbackend.model.Ticket;
+import com.ecxfoi.wbl.wienerbergerbackend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +9,6 @@ import org.springframework.stereotype.Repository;
 public interface TicketRepository extends JpaRepository<Ticket, Long>
 {
     Ticket findTicketByIdTicket(Long id);
+
+    Ticket findAllByTicketUser(User user);
 }
