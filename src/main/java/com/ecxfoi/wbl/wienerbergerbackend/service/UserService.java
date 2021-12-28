713ce0706245bc9c3a0ba867267f4a21a989f9f0
Lovrom8 <pamet.pijetlova@gmail.com>
2021-12-28 07:59:31 +0100

    WBL-62 Get old user data after mapping the update DTO

--- src/main/java/com/ecxfoi/wbl/wienerbergerbackend/service/UserService.java
+++ src/main/java/com/ecxfoi/wbl/wienerbergerbackend/service/UserService.java
@@ -8,26 +8,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService
-{
+public class UserService {
     private final UserMapper userMapper;
     private final UserRepository userRepository;
 
-    public UserService(@Autowired UserRepository userRepository)
-    {
+    public UserService(@Autowired UserRepository userRepository) {
         this.userRepository = userRepository;
         this.userMapper = new UserMapper();
     }
 
-    public UserDto getUserDto(Long ID)
-    {
+    public UserDto getUserDto(Long ID) {
         User user = userRepository.findUserById(ID);
         return userMapper.mapDto(user);
     }
 
-    public void saveUserDto(final UserDto userDto) throws Exception
-    {
-        User user = userMapper.map(userDto);
-        userRepository.save(user);
+    public void saveUserDto(final UserDto userDto) throws Exception {
+        User updatedUser = userMapper.map(userDto);
+
+        if (updatedUser.getId() != null) {
+            User oldUser = userRepository.getById(updatedUser.getId());
+            updatedUser.setPass(oldUser.getPass());
+        }
+
+        userRepository.save(updatedUser);
     }
 }
