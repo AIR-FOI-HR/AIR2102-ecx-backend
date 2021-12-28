c631bc04fa404c1c88612de4fdf5d37fe5ba6cb4
Lovrom8 <pamet.pijetlova@gmail.com>
2021-12-28 08:13:26 +0100

    WBL-62 Fixed formatting

--- src/main/java/com/ecxfoi/wbl/wienerbergerbackend/service/UserService.java
+++ src/main/java/com/ecxfoi/wbl/wienerbergerbackend/service/UserService.java
@@ -8,24 +8,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService
+{
     private final UserMapper userMapper;
     private final UserRepository userRepository;
 
-    public UserService(@Autowired UserRepository userRepository) {
+    public UserService(@Autowired UserRepository userRepository)
+    {
         this.userRepository = userRepository;
         this.userMapper = new UserMapper();
     }
 
-    public UserDto getUserDto(Long ID) {
+    public UserDto getUserDto(Long ID)
+    {
         User user = userRepository.findUserById(ID);
         return userMapper.mapDto(user);
     }
 
-    public void saveUserDto(final UserDto userDto) throws Exception {
+    public void saveUserDto(final UserDto userDto) throws Exception
+    {
         User updatedUser = userMapper.map(userDto);
 
-        if (updatedUser.getId() != null) {
+        if (updatedUser.getId() != null)
+        {
             User oldUser = userRepository.getById(updatedUser.getId());
             updatedUser.setPass(oldUser.getPass());
         }
